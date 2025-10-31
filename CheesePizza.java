3f94c0afa1e5197afa11c30e78db1ef8342d26bb
bs+ <solthings27@gmail.com>
2025-10-30 22:19:03 -0300

    Update CheesePizza.java
    Implement CheesePizza class with ingredient factory

--- CheesePizza.java
+++ CheesePizza.java
@@ -1,3 +1,15 @@
-public class CheesePizza {
-    
+public class CheesePizza extends Pizza {
+    PizzaIngredientFactory ingredientFactory;
+
+    public CheesePizza(PizzaIngredientFactory ingredientFactory) {
+        this.ingredientFactory = ingredientFactory;
+    }
+
+    void preparar() {
+        System.out.println("preparando" + name);
+        dough = ingredientFactory.createDough();
+        sauce = ingredientFactory.createSauce();
+        cheese = ingredientFactory.createCheese();
+
+    }
 }
